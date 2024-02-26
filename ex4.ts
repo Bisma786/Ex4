293bbaeb42fbca25de84c5c5c4ae0ca393e9f23f
Bisma786 <159830145+Bisma786@users.noreply.github.com>
2024-02-26 17:34:24 +0500

    Initial commit

---
+++ ex4.ts
@@ -0,0 +1,5 @@
+// Q:4 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+console.log(`Andrew Tate once said, "Do the impossible and you'll never doubt yourself again"`)
